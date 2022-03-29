25d6bdf808c8aeb43dc930abb40df9f581249c04
Hannes Fassold <hfassold@gmail.com>
2022-03-29 11:42:09 +0200

    Update jrs_run_training.bat

--- jrs_run_training.bat
+++ jrs_run_training.bat
@@ -1,3 +1,3 @@
 REM call conda activate textclassify
 
-python--max_epochs 10 --nr_frozen_epochs 0 --batch_size 64 --learning_rate 1e-4 --seed 42
+python nlp_finetuning_lightning_google.py --max_epochs 10 --nr_frozen_epochs 0 --batch_size 64 --learning_rate 1e-4 --seed 42
